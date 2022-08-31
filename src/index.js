d93656b9be8de200f69ea136cf4d7dda863489ce
Harold <haroldbarron13@gmail.com>
2022-08-31 17:12:28 -0500

    creating chat.js and adding scripts to index.html to conect whith server

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
 const port = process.env.PORT || 3000
 
 const publcicDirectoryPath = path.join(__dirname,'../public')
 
 app.use(express.static(publcicDirectoryPath))
 
-app.get('',(req,res)=>{
-    res.render({
-        title:"Chat app"
-    })
+io.on('connection', ()=>{
+    console.log('new websocket connection')
 })
-app.listen(port, ()=>{
+
+server.listen(port, ()=>{
     console.log(`Listening on port ${port}`)
 })
