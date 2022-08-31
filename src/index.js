41fe8db292f5a039dac9bca6a1b4408e5f5937fd
Harold <haroldbarron13@gmail.com>
2022-08-31 17:59:55 -0500

    learning fundamentals of socket io

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
 
+
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
@@ -12,9 +13,16 @@ const port = process.env.PORT || 3000
 const publcicDirectoryPath = path.join(__dirname,'../public')
 
 app.use(express.static(publcicDirectoryPath))
-
-io.on('connection', ()=>{
+let count =0
+io.on('connection', (socket)=>{
     console.log('new websocket connection')
+    socket.emit('countUpdated',count)
+
+    socket.on('increment', ()=>{
+        count++
+        // socket.emit('countUpdated',count) //emit to an specific connection
+        io.emit('countUpdated',count) //emit to all connections
+    })
 })
 
 server.listen(port, ()=>{
