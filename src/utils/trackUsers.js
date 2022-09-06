a669b75813a33595cf6eb3018c2095386054b981
Harold <haroldbarron13@gmail.com>
2022-09-06 18:57:21 -0500

    Creatin removeUser function

--- src/utils/trackUsers.js
+++ src/utils/trackUsers.js
@@ -35,10 +35,11 @@ const addUser = ({id,username,room})=>{
     return(user)
 }
 
-addUser({
-    id:22,
-    username:'harold',
-    room:'haroldroom'
-})
+const removeUser = (id) =>{
+    const index = users.findIndex((user)=>user.id === id)
 
+    if(!index === -1){
+        return users.splice(index,1)[0]
+    }
 
+}
