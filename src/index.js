f0d4dcde08481c373e10d1a8082c7f82e5e106af
Harold <haroldbarron13@gmail.com>
2022-09-12 19:36:34 -0500

    Deleting room name as a requirement to create a new user

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const socketio = require('socket.io')
 const Filter = require('bad-words')
 const {generateMessage, generateLocationMessage}= require('./utils/messages')
 const {addUser,removeUser,getUser,getUsersInRoom} = require('./utils/trackUsers')
-const {getRooms,deleteRooms,aviableRooms} = require('./utils/trackRooms')
+const {getRooms,deleteRooms,aviableRooms,getSelectedRoom,sendNewRoom,deleteNewRoom} = require('./utils/trackRooms')
 
 const app = express()
 const server = http.createServer(app)
@@ -15,15 +15,45 @@ const port = process.env.PORT || 3000
 
 const publcicDirectoryPath = path.join(__dirname,'../public')
 
+
 app.use(express.static(publcicDirectoryPath))
 
 io.on('connection', (socket)=>{
     console.log('new websocket connection')
     const rooms = aviableRooms()
+    // const roomSelected =[]
     socket.emit('joiningPage',rooms)
-
+    socket.on('roomSelected', (newroomSelected)=>{
+        getSelectedRoom(newroomSelected)
+    })
+    
     socket.on('join', (options,callback)=>{
+        // console.log(options)
+        const newRoom = sendNewRoom()
+        // console.log(newRoom)
         const {error,user} = addUser({id:socket.id, ...options})
+        if(user && newRoom){
+            // console.log(newRoom[0])
+            const users1 = getUsersInRoom(newRoom[0])
+            // console.log(users1)
+            const repeatedName = users1.find((usersOnline)=>{
+                return usersOnline.username === user.username
+            })
+            if(repeatedName){
+                socket.emit('repeatedName', 'Username in use')
+                // return console.log('name',repeatedName)
+            }
+            if(user.room && newRoom != user.room){
+                
+                socket.emit('repeatedName', 'Please select the room that you want or text a new one')
+            }
+            if(newRoom && !user.room){
+                user.room = newRoom[0]
+            }
+        }
+        if(!user.room){
+            socket.emit('repeatedName', 'Please text the room name')
+        }
         if(user){
             const aviableRooms = getRooms(user.room)
         }
@@ -33,7 +63,7 @@ io.on('connection', (socket)=>{
         }
 
         socket.join(user.room)
-
+        deleteNewRoom()
         socket.emit('message',generateMessage('Admin',`Welcome ${user.username}!`))
         socket.broadcast.to(user.room).emit('message',generateMessage('Admin',`${user.username} has joined!` ))
 
@@ -64,7 +94,7 @@ io.on('connection', (socket)=>{
 
     socket.on('disconnect',()=>{
         const user = removeUser(socket.id)
-        console.log(user)
+        // console.log(user)
         if(user){
             const users = getUsersInRoom(user.room)
             io.to(user.room).emit('message',generateMessage('Admin',`${user.username} has left`))
@@ -78,8 +108,6 @@ io.on('connection', (socket)=>{
 
 
 })
-
-
 
 server.listen(port, ()=>{
     console.log(`Listening on port ${port}`)
